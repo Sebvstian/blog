228b8a0da7a712c59f6389b319970223cfe48fcb
Sebvstian <sgarrcia@hotmail.com>
2024-12-07 11:11:20 -0400

    Update and Delete

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic import ListView, DetailView 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
-
+from django.urls import reverse_lazy
+# si ponemos un * nos importa todo de una
 # Create your views here.
 
 class PostListView(ListView):
@@ -18,6 +19,16 @@ class PostCreateView(CreateView):
         template_name = 'post_new.html'
         fields = ['title', 'content', 'author']
         
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title', 'content']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'  
+    success_url = reverse_lazy('blog-home')
 
 
 
