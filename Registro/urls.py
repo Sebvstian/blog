d48352f80a8d898a96f5c84ef63693d38ef6ea6e
Sebvstian <sgarrcia@hotmail.com>
2024-12-14 10:37:15 -0400

    Login

--- Registro/urls.py
+++ Registro/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include 
-from Registro.views import SignUpView
+from Registro.views import SignUpView, Home
 
 urlpatterns=[
 
     path("accounts/",include('django.contrib.auth.urls')),
-    path("", SignUpView.as_view(), name="signup")
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("",Home, name="Home"),
 
 ]
--- Registro/views.py
+++ Registro/views.py
@@ -9,3 +9,8 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login') 
     template_name = 'registration/signup.html'
+
+def Home(request):
+    return render(request, 'Home.html')
+
+
